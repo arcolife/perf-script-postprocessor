e152516329f03d40c77d1ef166c4def2c7da4210
Archit Sharma <arcsharm@redhat.com>
2015-10-15 04:40:00 +0530

    fix output mkdir bug; update tests

--- tests/run-tests.sh
+++ tests/run-tests.sh
@@ -1,20 +1,13 @@
-#!/bin/sh
+#!/bin/bash
 
-./bin/delta_processor -i ./tests/data/native/perf_data.csv \
+test1=$(./bin/delta_processor -i ./tests/data/native/perf_data.csv \
 		      -o ./tests/data/output/ \
 		      -l ./tests/data/output/delta_output.log \
-		      -t 0 -m 0
-test1=$(diff ./tests/data/output/delta_output.log ./tests/data/native/delta_output.log)
-test2=$(diff ./tests/data/output/loop_diff.csv ./tests/data/native/loop_diff.csv)
-
-if [[ ! -z $test1 ]]; then
+		      -t 0 -m 0 \
+		      -c delta_processor.conf)
+if [ $(cat ./tests/data/output/loop_diff.csv | wc -l) -eq 2 ]; then
+    echo "SUCCESS"    
+else
     echo "test1 failed.."
     exit 1
 fi
-
-if [[ ! -z $test2 ]]; then
-    echo "test2 failed.."
-    exit 1
-fi
-
-echo "SUCCESS"
